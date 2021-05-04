f8d0a8c2fe20fcafcce60d0acde64127d37e24e9
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-05-04 12:07:08 -0300

    refactor(backend): delete the backend project

--- backend/src/test/kotlin/factories/Factory.kt
+++
@@ -1,9 +0,0 @@
-package com.lorenzoog.diekeditora.factories
-
-interface Factory<T> {
-    fun generateMany(amount: Int = 5): List<T> = (0..amount).map {
-        generateEntity()
-    }
-
-    fun generateEntity(): T
-}
