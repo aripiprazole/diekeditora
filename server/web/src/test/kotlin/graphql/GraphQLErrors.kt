1030bc97bb69cd8ec066a11a3e9431a2b5b84b36
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-06-03 10:35:39 -0300

    test(server): test not having permission also

---
+++ server/web/src/test/kotlin/graphql/GraphQLErrors.kt
@@ -0,0 +1,4 @@
+package com.diekeditora.web.tests.graphql
+
+const val NotEnoughAuthorities =
+    "Request failed with: Exception while fetching data (/user) : Not enough permissions"
--- server/web/src/test/kotlin/graphql/user/UserMutationTest.kt
+++ server/web/src/test/kotlin/graphql/user/UserMutationTest.kt
@@ -4,11 +4,14 @@ import com.diekeditora.domain.user.UserInput
 import com.diekeditora.infra.repositories.UserRepository
 import com.diekeditora.web.graphql.user.UpdateUserInput
 import com.diekeditora.web.tests.factories.UserFactory
+import com.diekeditora.web.tests.graphql.GraphQLException
 import com.diekeditora.web.tests.graphql.GraphQLTestClient
+import com.diekeditora.web.tests.graphql.NotEnoughAuthorities
 import com.diekeditora.web.tests.graphql.request
 import com.diekeditora.web.tests.utils.AuthenticationMocker
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.assertEquals
@@ -21,6 +24,7 @@ class UserMutationTest(
     @Autowired private val userFactory: UserFactory,
     @Autowired private val auth: AuthenticationMocker,
 ) {
+
     @Test
     fun `test should create user`(): Unit = runBlocking {
         var user = userFactory.create()
@@ -46,6 +50,20 @@ class UserMutationTest(
     }
 
     @Test
+    fun `test should not create user without authorities`(): Unit = runBlocking {
+        val user = userFactory.create()
+
+        assertThrows<GraphQLException>(NotEnoughAuthorities) {
+            client.request(CreateUserMutation) {
+                authentication = auth.mock()
+                variables = CreateUserMutation.Variables(
+                    input = UserInput.from(user),
+                )
+            }
+        }
+    }
+
+    @Test
     fun `test should update user by username`(): Unit = runBlocking {
         var user = userFactory.create().let { userRepository.save(it) }
         val newUser = userFactory.create()
@@ -66,5 +84,20 @@ class UserMutationTest(
         assertEquals(name, user.name)
         assertEquals(username, user.username)
         assertEquals(email, user.email)
+    }
+
+    @Test
+    fun `test should not update user by username without authorities`(): Unit = runBlocking {
+        val user = userFactory.create().let { userRepository.save(it) }
+        val newUser = userFactory.create()
+
+        assertThrows<GraphQLException>(NotEnoughAuthorities) {
+            client.request(UpdateUserMutation) {
+                authentication = auth.mock()
+                variables = UpdateUserMutation.Variables(
+                    input = UpdateUserInput(user.username, UserInput.from(newUser)),
+                )
+            }
+        }
     }
 }
