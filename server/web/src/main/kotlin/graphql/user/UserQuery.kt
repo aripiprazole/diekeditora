a99b01cae09a25e9ed1ff5ac4b8c8e435a325c94
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-06-03 05:22:24 -0300

    feat(server): authenticate graphql requests

--- server/web/src/main/kotlin/graphql/user/UserQuery.kt
+++ server/web/src/main/kotlin/graphql/user/UserQuery.kt
@@ -1,22 +1,25 @@
 package com.diekeditora.web.graphql.user
 
-import com.expediagroup.graphql.generator.annotations.GraphQLDescription
-import com.expediagroup.graphql.server.operations.Query
 import com.diekeditora.domain.user.User
 import com.diekeditora.domain.user.UserService
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import com.expediagroup.graphql.server.operations.Query
 import graphql.relay.Connection
 import graphql.relay.SimpleListConnection
 import graphql.schema.DataFetchingEnvironment
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Component
 
 @Component
 class UserQuery(val userService: UserService) : Query {
     @GraphQLDescription("Returns user's details by its username")
+    @PreAuthorize("hasAuthority('users.view')")
     suspend fun user(username: String): User? {
         return userService.findUserByUsername(username)
     }
 
     @GraphQLDescription("Returns user page")
+    @PreAuthorize("hasAuthority('users.view')")
     suspend fun users(page: Int, env: DataFetchingEnvironment): Connection<User> {
         val (items) = userService.findPaginatedUsers(page)
 
