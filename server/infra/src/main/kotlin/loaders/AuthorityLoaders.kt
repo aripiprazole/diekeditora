a56823fad57cc80d166e2fb5f04bf52a27c07e84
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-06-14 15:44:38 -0300

    feat(server): use dataloaders for user's authorities and roles

--- server/infra/src/main/kotlin/loaders/AuthorityLoaders.kt
+++ server/infra/src/main/kotlin/loaders/AuthorityLoaders.kt
@@ -4,6 +4,8 @@ package com.diekeditora.infra.loaders
 
 import com.diekeditora.domain.authority.Role
 import com.diekeditora.domain.authority.RoleService
+import com.diekeditora.domain.user.User
+import com.diekeditora.domain.user.UserService
 import com.diekeditora.shared.dataLoader
 import com.expediagroup.graphql.server.execution.KotlinDataLoader
 import org.springframework.stereotype.Component
@@ -12,4 +14,10 @@ import org.springframework.stereotype.Component
 class RoleAuthorityLoader(val roleService: RoleService) :
     KotlinDataLoader<Role, List<String>> by dataLoader("RoleAuthorityLoader").execute({ role ->
         roleService.findRoleAuthorities(role)
+    })
+
+@Component
+class UserAuthorityLoader(val userService: UserService) :
+    KotlinDataLoader<User, List<String>> by dataLoader("UserAuthorityLoader").execute({ user ->
+        userService.findUserAuthorities(user)
     })
