df5c76d5befdb7d217eccb7dbe48bd4f15ebafbe
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-06-16 16:09:09 -0300

    fix(server): remove primary key from relationship tables

--- server/web/src/test/resources/db/migration/0004_create_role_authority_table.sql
+++ server/web/src/test/resources/db/migration/0004_create_role_authority_table.sql
@@ -1,6 +1,5 @@
 create table if not exists "role_authority"
 (
     role_id   uuid        not null,
-    authority varchar(80) not null,
-    primary key (role_id)
+    authority varchar(80) not null
 );
