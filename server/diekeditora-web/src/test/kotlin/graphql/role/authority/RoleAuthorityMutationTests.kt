c1fbb68195fa6c2982329242aafb57b62a1d611b
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-06-16 18:01:15 -0300

    feat(server): implement role authority mutation

---
+++ server/diekeditora-web/src/test/kotlin/graphql/role/authority/RoleAuthorityMutationTests.kt
@@ -0,0 +1,100 @@
+package com.diekeditora.web.tests.graphql.role.authority
+
+import com.diekeditora.infra.repositories.RoleAuthorityRepository
+import com.diekeditora.infra.repositories.RoleRepository
+import com.diekeditora.web.tests.factories.AuthorityFactory
+import com.diekeditora.web.tests.factories.RoleFactory
+import com.diekeditora.web.tests.graphql.GraphQLTestClient
+import com.diekeditora.web.tests.graphql.request
+import com.diekeditora.web.tests.utils.AuthenticationMocker
+import com.diekeditora.web.tests.utils.assertGraphQLForbidden
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
+
+@SpringBootTest
+class RoleAuthorityMutationTests(
+    @Autowired val roleRepository: RoleRepository,
+    @Autowired val roleFactory: RoleFactory,
+    @Autowired val authorityFactory: AuthorityFactory,
+    @Autowired val roleAuthorityRepository: RoleAuthorityRepository,
+    @Autowired val client: GraphQLTestClient,
+    @Autowired val auth: AuthenticationMocker,
+) {
+    @Test
+    fun `test should link an authority to role`(): Unit = runBlocking {
+        val role = roleRepository.save(roleFactory.create())
+        val authority = authorityFactory.create()
+
+        val authorities = roleAuthorityRepository.findByRole(role).toList().map { it.value }
+
+        client.request(LinkRoleAuthoritiesQuery) {
+            authentication = auth.mock("authority.admin", "authority.view")
+            variables = LinkRoleAuthoritiesQuery.Variables(
+                name = role.name,
+                authorities = listOf(authority.value),
+            )
+        }
+
+        assertEquals(
+            authorities + authority.value,
+            roleAuthorityRepository.findByRole(role).toList().map { it.value }
+        )
+    }
+
+    @Test
+    fun `test should not link an authority to role without authorities`(): Unit = runBlocking {
+        val role = roleRepository.save(roleFactory.create())
+        val authority = authorityFactory.create()
+
+        assertGraphQLForbidden {
+            client.request(LinkRoleAuthoritiesQuery) {
+                authentication = auth.mock()
+                variables = LinkRoleAuthoritiesQuery.Variables(
+                    name = role.name,
+                    authorities = listOf(authority.value),
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `test should unlink an authority from role`(): Unit = runBlocking {
+        val role = roleRepository.save(roleFactory.create())
+        val authority = authorityFactory.create().also { roleAuthorityRepository.link(role, it) }
+
+        val authorities = roleAuthorityRepository.findByRole(role).toList().map { it.value }
+
+        client.request(UnlinkRoleAuthoritiesQuery) {
+            authentication = auth.mock("authority.admin", "authority.view")
+            variables = UnlinkRoleAuthoritiesQuery.Variables(
+                name = role.name,
+                authorities = listOf(authority.value),
+            )
+        }
+
+        assertEquals(
+            authorities - authority.value,
+            roleAuthorityRepository.findByRole(role).toList().map { it.value }
+        )
+    }
+
+    @Test
+    fun `test should not unlink an authority from role without authorities`(): Unit = runBlocking {
+        val role = roleRepository.save(roleFactory.create())
+        val authority = authorityFactory.create().also { roleAuthorityRepository.link(role, it) }
+
+        assertGraphQLForbidden {
+            client.request(UnlinkRoleAuthoritiesQuery) {
+                authentication = auth.mock()
+                variables = UnlinkRoleAuthoritiesQuery.Variables(
+                    name = role.name,
+                    authorities = listOf(authority.value),
+                )
+            }
+        }
+    }
+}
