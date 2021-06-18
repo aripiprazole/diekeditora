8b68ca7d1b15d90b8f37d12d97f1c954aaf3f3df
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-06-18 18:46:54 -0300

    refactor(web): restructure frontend project

---
+++ web/packages/app/next.config.js
@@ -0,0 +1,5 @@
+const withTM = require('next-transpile-modules')(['@diekeditora/ui']);
+
+module.exports = withTM({
+  reactStrictMode: true,
+});
--- web/store/index.ts
+++
@@ -1,5 +0,0 @@
-import { configure } from 'mobx';
-
-configure({ useProxies: 'never' });
-
-export { default as auth } from './auth';
