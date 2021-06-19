0bdb1e34ca272733be0ebb8f206b1319e0a0f4af
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-06-19 13:58:51 -0300

    feat(web): add storybook for @diekeditora/ui

---
+++ web/packages/ui/components/Button/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './index.css';
+
+export type ButtonProps = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+
+  /**
+   * Button contents
+   */
+  label: string;
+
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+
+  /**
+   * Optional click handler
+   */
+  onClick: () => void;
+};
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button: React.FC<ButtonProps> = ({ size = 'medium', primary, backgroundColor, label, ...props }) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
