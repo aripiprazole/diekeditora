10f82a6a837362ecbd4035ee2e75411091f06f8b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-01 08:46:31 -0300

    feat(server): add notification service and entity

---
+++ server/diekeditora-domain/src/main/kotlin/notification/NotificationService.kt
@@ -0,0 +1,12 @@
+package com.diekeditora.domain.notification
+
+import com.diekeditora.domain.id.UniqueId
+import com.diekeditora.domain.page.Page
+import com.diekeditora.domain.user.User
+import kotlinx.coroutines.flow.Flow
+
+interface NotificationService {
+    suspend fun findNotifications(user: User, first: Int, after: UniqueId): Page<Notification>
+
+    suspend fun subscribeNotifications(user: User): Flow<Notification>
+}
