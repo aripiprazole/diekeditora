fb4a1a0a7eac198590e603611508b793b1939b11
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-07-01 07:18:11 -0300

    feat(web): add admin panel base from https://github.com/minimal-ui-kit/material-kit-react

--- server/diekeditora-domain/src/main/kotlin/manga/MangaSearchService.kt
+++
@@ -1,7 +0,0 @@
-package com.diekeditora.domain.manga
-
-import com.diekeditora.domain.page.Page
-
-interface MangaSearchService {
-    suspend fun searchMangas(genres: Set<String>, sort: MangaSort = MangaSort.empty()): Page<Manga>
-}
