113d47999ed2735113751975cc0b01e55df3e110
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-01 08:47:17 -0300

    refactor(server): add Mutable prefix in Entity interface

--- server/diekeditora-domain/src/main/kotlin/MutableEntity.kt
+++ server/diekeditora-domain/src/main/kotlin/MutableEntity.kt
@@ -3,7 +3,7 @@ package com.diekeditora.domain
 import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
 
 @GraphQLIgnore
-interface Entity<T> where T : Entity<T>, T : Any {
+interface MutableEntity<T> where T : MutableEntity<T>, T : Any {
     @GraphQLIgnore
     fun update(with: T): T
 }
