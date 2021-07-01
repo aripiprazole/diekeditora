5aa1c4845d418232acdb3e301cba36a367adb378
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-01 08:46:22 -0300

    feat(server): add notification mutation/query

---
+++ server/diekeditora-web/src/main/kotlin/graphql/me/NotificationSubscription.kt
@@ -0,0 +1,17 @@
+package com.diekeditora.web.graphql.me
+
+import com.diekeditora.domain.notification.Notification
+import com.diekeditora.domain.notification.NotificationService
+import com.diekeditora.web.graphql.AuthGraphQLContext
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import com.expediagroup.graphql.server.operations.Subscription
+import kotlinx.coroutines.flow.Flow
+import org.springframework.stereotype.Component
+
+@Component
+class NotificationSubscription(val notificationService: NotificationService) : Subscription {
+    @GraphQLDescription("Subscribes current user for its new notifications")
+    suspend fun notificationAdded(ctx: AuthGraphQLContext): Flow<Notification> {
+        return notificationService.subscribeNotifications(ctx.user)
+    }
+}
