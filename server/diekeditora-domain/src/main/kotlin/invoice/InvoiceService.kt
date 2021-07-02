de819f5a8eaeda222d8ad93796aa2bb7023b1127
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-02 10:49:24 -0300

    feat(server): add page search for invoices

--- server/diekeditora-domain/src/main/kotlin/invoice/InvoiceService.kt
+++ server/diekeditora-domain/src/main/kotlin/invoice/InvoiceService.kt
@@ -1,12 +1,26 @@
 package com.diekeditora.domain.invoice
 
+import com.diekeditora.domain.id.UniqueId
 import com.diekeditora.domain.manga.Chapter
 import com.diekeditora.domain.manga.Manga
+import com.diekeditora.domain.page.Page
 import kotlinx.coroutines.flow.Flow
 
 interface InvoiceService {
+    suspend fun findAllMangaInvoices(manga: Manga, first: Int, after: UniqueId): Page<Invoice>
     suspend fun subscribeAllMangaInvoices(manga: Manga): Flow<Invoice>
 
     suspend fun subscribeMangaSubscriptions(manga: Manga): Flow<MangaSubscriptionInvoice>
-    suspend fun subscribeChapterSells(chapter: Chapter): Flow<MangaChapterInvoice>
+    suspend fun findMangaSubscriptions(
+        manga: Manga,
+        first: Int,
+        after: UniqueId
+    ): Page<MangaSubscriptionInvoice>
+
+    suspend fun subscribeChapterInvoices(chapter: Chapter): Flow<MangaChapterInvoice>
+    suspend fun findChapterInvoices(
+        chapter: Chapter,
+        first: Int,
+        after: UniqueId
+    ): Page<MangaChapterInvoice>
 }
