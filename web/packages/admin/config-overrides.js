4db17916e51f24093e0e882f940638d717bd7653
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-04 09:41:49 -0300

    fix(web): fix relative paths not working in @diekeditora/admin

--- web/packages/admin/config-overrides.js
+++ web/packages/admin/config-overrides.js
@@ -1,11 +1,20 @@
 const JsconfigPathsPlugin = require('jsconfig-paths-webpack-plugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
-module.exports = function override(config) {
-  config.resolve.plugins = config.resolve.plugins.filter(
-    (plugin) => !(plugin instanceof ModuleScopePlugin)
-  );
-  config.resolve.plugins.push(new JsconfigPathsPlugin());
+module.exports = {
+  webpack(config) {
+    config.resolve.plugins = config.resolve.plugins.filter(
+      (plugin) => !(plugin instanceof ModuleScopePlugin)
+    );
+    config.resolve.plugins.push(new JsconfigPathsPlugin());
 
-  return config;
+    return config;
+  },
+
+  paths(paths) {
+    paths['@diekeditora/admin'] = './src';
+    paths['@diekeditora/admin/*'] = './src/*';
+
+    return paths;
+  }
 };
