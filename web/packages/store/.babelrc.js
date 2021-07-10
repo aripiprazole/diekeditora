8eaa9213f329a36e32dae5c29702eb82ecefb8fd
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-10 12:02:47 -0300

    feat(web): add module resolver in babel

--- web/packages/store/.babelrc.js
+++ web/packages/store/.babelrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: { '~/': './' },
+      },
+    ],
+  ],
   plugins: ['@babel/plugin-transform-typescript'],
 };
