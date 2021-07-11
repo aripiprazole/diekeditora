e46dc83886fa9a9ce413630522624d4e37d2706d
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-11 18:14:21 -0300

    feat(web): add auth sagas

--- web/packages/store/src/modules/auth/sagas.ts
+++ web/packages/store/src/modules/auth/sagas.ts
@@ -1,3 +1,27 @@
-import {all} from 'redux-saga/effects';
+import {all, call, put} from 'redux-saga/effects';
+import {graphQLClient} from '~/client';
+import {User} from '~/entities';
+import {ME_USER_QUERY} from '~/queries';
 
-export const authSagas = all([]);
+import {AuthActions, LoginAction, loginFail, loginSuccess} from './actions';
+
+type LoginResponse = {
+  user: User;
+};
+
+/** Login saga */
+function* loginSaga(login: LoginAction) {
+  try {
+    const response: LoginResponse = yield call(() =>
+      graphQLClient.query({
+        query: ME_USER_QUERY,
+      }),
+    );
+
+    yield put(loginSuccess(response.user));
+  } catch (error) {
+    yield put(loginFail(error));
+  }
+}
+
+export const authSagas = all([AuthActions.LOGIN, loginSaga]);
