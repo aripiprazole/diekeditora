454e88f6f168dd2f3524d2715bd11c2bae248640
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-11 21:57:42 -0300

    feat(web): add single user fetch in user module

--- web/packages/store/src/modules/user/reducer.ts
+++ web/packages/store/src/modules/user/reducer.ts
@@ -4,12 +4,14 @@ import {Connection, User} from '~/entities';
 import {UserAction, UserActions} from './actions';
 
 export type UserState = {
+  user: User | null;
   connection: Connection<User> | null;
   error: Error | null;
   loading: boolean;
 };
 
 const defaultState: UserState = {
+  user: null,
   connection: null,
   error: null,
   loading: false,
@@ -18,19 +20,30 @@ const defaultState: UserState = {
 export const userReducer = (state = defaultState, action: UserAction) =>
   produce(state, (draft) => {
     switch (action.type) {
+      case UserActions.FETCH_USER:
       case UserActions.FETCH_USER_CONNECTION:
+        draft.user = null;
         draft.loading = true;
         draft.connection = null;
         draft.error = null;
         break;
 
       case UserActions.FETCH_USER_SUCCESS:
+        draft.user = action.payload.user;
+        draft.loading = false;
+        draft.connection = null;
+        draft.error = null;
+        break;
+
+      case UserActions.FETCH_USER_CONNECTION_SUCCESS:
+        draft.user = null;
         draft.loading = false;
         draft.connection = action.payload.connection;
         draft.error = null;
         break;
 
       case UserActions.FETCH_USER_FAIL:
+        draft.user = null;
         draft.loading = false;
         draft.connection = null;
         draft.error = action.payload.error;
--- web/packages/store/src/modules/user/sagas.ts
+++ web/packages/store/src/modules/user/sagas.ts
@@ -1,28 +1,59 @@
 import {all, call, put, takeEvery} from 'redux-saga/effects';
-import {graphQLClient} from '~/client';
 
+import {graphQLClient} from '~/client';
 import {Connection, User} from '~/entities';
-import {USERS_QUERY} from '~/queries';
-import {FetchUserConnectionAction, fetchUserFail, fetchUserSuccess, UserActions} from './actions';
+import {USER_CONNECTION_QUERY} from '~/queries';
+
+import {
+  FetchUserConnectionAction,
+  fetchUserFail,
+  fetchUserConnectionSuccess,
+  UserActions,
+  FetchUserAction,
+  fetchUserSuccess,
+} from './actions';
 
 type FetchUserConnectionResponse = {
   users: Connection<User>;
 };
 
-/** Fetch users saga */
+type FetchUserResponse = {
+  user: User;
+};
+
+/** Fetch user connection saga */
 function* fetchUserConnectionSaga(action: FetchUserConnectionAction) {
   try {
     const response: FetchUserConnectionResponse = yield call(() =>
       graphQLClient.query({
-        query: USERS_QUERY,
+        query: USER_CONNECTION_QUERY,
         variables: action.payload,
       }),
     );
 
-    yield put(fetchUserSuccess(response.users));
+    yield put(fetchUserConnectionSuccess(response.users));
   } catch (error) {
     yield put(fetchUserFail(error));
   }
 }
 
-export const userSagas = all([takeEvery(UserActions.FETCH_USER_CONNECTION, fetchUserConnectionSaga)]);
+/** Fetch user saga */
+function* fetchUserSaga(action: FetchUserAction) {
+  try {
+    const response: FetchUserResponse = yield call(() =>
+      graphQLClient.query({
+        query: USER_CONNECTION_QUERY,
+        variables: action.payload,
+      }),
+    );
+
+    yield put(fetchUserSuccess(response.user));
+  } catch (error) {
+    yield put(fetchUserFail(error));
+  }
+}
+
+export const userSagas = all([
+  takeEvery(UserActions.FETCH_USER_CONNECTION, fetchUserConnectionSaga),
+  takeEvery(UserActions.FETCH_USER, fetchUserSaga),
+]);
