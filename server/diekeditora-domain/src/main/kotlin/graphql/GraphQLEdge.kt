cfcf689a1e43d1d622ceda05f96e485b5a53a003
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-13 17:12:28 -0300

    refactor(server): remove old graphql dtos

--- server/diekeditora-domain/src/main/kotlin/graphql/GraphQLEdge.kt
+++
@@ -1,9 +0,0 @@
-package com.diekeditora.domain.graphql
-
-import graphql.relay.ConnectionCursor
-import graphql.relay.Edge
-
-class GraphQLEdge<T>(private val node: T, private val cursor: ConnectionCursor) : Edge<T> {
-    override fun getNode() = node
-    override fun getCursor() = cursor
-}
