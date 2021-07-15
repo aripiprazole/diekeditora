ecbfbc817c690392c7d8390128f460ff22493aeb
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-07-14 21:16:28 -0300

    fix(server): fix implementations of loaders

--- server/diekeditora-infra/src/main/kotlin/loaders/AuthorityLoaders.kt
+++ server/diekeditora-infra/src/main/kotlin/loaders/AuthorityLoaders.kt
@@ -3,28 +3,31 @@
 package com.diekeditora.infra.loaders
 
 import com.diekeditora.domain.authority.AuthorityService
+import com.diekeditora.domain.dataloader.PaginationArg
 import com.diekeditora.domain.role.Role
-import com.diekeditora.domain.role.RoleService
 import com.diekeditora.domain.user.User
-import com.diekeditora.domain.user.UserService
 import com.diekeditora.shared.dataLoader
 import com.expediagroup.graphql.server.execution.KotlinDataLoader
+import graphql.relay.Connection
 import org.springframework.stereotype.Component
 
 @Component
-class RoleAuthorityLoader(val roleService: RoleService) :
-    KotlinDataLoader<Role, Set<String>> by dataLoader("RoleAuthorityLoader").execute({ role ->
-        roleService.findRoleAuthorities(role)
-    })
+class RoleAuthorityLoader(val authorityService: AuthorityService) :
+    KotlinDataLoader<PaginationArg<Role, String>, Connection<String>> by dataLoader("RoleAuthoritiesLoader")
+        .execute({ (role, first, after) ->
+            authorityService.findAuthoritiesByRole(role, first, after)
+        })
 
 @Component
-class UserAuthorityLoader(val userService: UserService) :
-    KotlinDataLoader<User, Set<String>> by dataLoader("UserAuthorityLoader").execute({ user ->
-        userService.findUserAuthorities(user)
-    })
+class UserAuthorityLoader(val authorityService: AuthorityService) :
+    KotlinDataLoader<PaginationArg<User, String>, Connection<String>> by dataLoader("UserAuthoritiesLoader")
+        .execute({ (user, first, after) ->
+            authorityService.findAuthoritiesByUser(user, first, after)
+        })
 
 @Component
 class AllUserAuthorityLoader(val authorityService: AuthorityService) :
-    KotlinDataLoader<User, Set<String>> by dataLoader("AllUserAuthorityLoader").execute({ user ->
-        authorityService.findAllAuthoritiesByUser(user)
-    })
+    KotlinDataLoader<PaginationArg<User, String>, Connection<String>> by dataLoader("UserAllAuthoritiesLoader")
+        .execute({ (user, first, after) ->
+            authorityService.findAuthoritiesByUser(user, first, after)
+        })
