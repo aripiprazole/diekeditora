932ca18436fc2f5290279d1c8f51d9722e777f33
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-14 21:16:28 -0300

    fix(server): fix implementations of loaders

--- server/diekeditora-infra/src/main/kotlin/loaders/UserRoleLoader.kt
+++ server/diekeditora-infra/src/main/kotlin/loaders/UserRoleLoader.kt
@@ -1,14 +1,17 @@
 package com.diekeditora.infra.loaders
 
+import com.diekeditora.domain.dataloader.PaginationArg
 import com.diekeditora.domain.role.Role
+import com.diekeditora.domain.role.RoleService
 import com.diekeditora.domain.user.User
-import com.diekeditora.domain.user.UserService
 import com.diekeditora.shared.dataLoader
 import com.expediagroup.graphql.server.execution.KotlinDataLoader
+import graphql.relay.Connection
 import org.springframework.stereotype.Component
 
 @Component
-class UserRoleLoader(val userService: UserService) :
-    KotlinDataLoader<User, Set<Role>> by dataLoader("UserRoleLoader").execute({ user ->
-        userService.findUserRoles(user)
-    })
+class UserRoleLoader(val roleService: RoleService) :
+    KotlinDataLoader<PaginationArg<User, String>, Connection<Role>> by dataLoader("UserRolesLoader")
+        .execute({ (user, first, after) ->
+            roleService.findRolesByUser(user, first, after)
+        })
