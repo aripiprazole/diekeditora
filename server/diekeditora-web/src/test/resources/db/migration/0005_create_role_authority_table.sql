2701550253d050eb18f0f8db0d8b69803cbb7b4e
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-07-15 23:42:51 -0300

    refactor(server): assemble into one file all migration files

--- server/diekeditora-web/src/test/resources/db/migration/0005_create_role_authority_table.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists "role_authority"
-(
-    role_id      uuid not null,
-    authority_id uuid not null
-);
--- server/diekeditora-web/src/test/resources/db/migration/0007_create_user_role_table.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists "user_role"
-(
-    user_id uuid not null,
-    role_id uuid not null
-);
