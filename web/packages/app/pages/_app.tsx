8ce20e1ef2fd18562b67dfb4238aecbac530b0fb
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-17 22:26:22 -0300

    fix(web): fix app startup

--- web/packages/app/pages/_app.tsx
+++ web/packages/app/pages/_app.tsx
@@ -2,18 +2,8 @@ import React from 'react';
 
 import {AppProps} from 'next/app';
 
-import {ThemeProvider} from 'styled-components';
-
-import {GlobalStyle} from '@diekeditora/ui/styles';
-import {defaultTheme} from '@diekeditora/ui/theme';
-
 const MyApp: React.VFC<AppProps> = ({Component, pageProps}) => {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
