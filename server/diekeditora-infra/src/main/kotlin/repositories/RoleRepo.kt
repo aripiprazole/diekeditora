6c6ee9d3509b79d8434d18e1baa2d1fb0bf2ff65
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-07-19 02:06:46 -0300

    refactor(server): add coroutine paging repository

--- server/diekeditora-infra/src/main/kotlin/repositories/RoleRepo.kt
+++ server/diekeditora-infra/src/main/kotlin/repositories/RoleRepo.kt
@@ -3,14 +3,13 @@ package com.diekeditora.infra.repositories
 import com.diekeditora.domain.role.Role
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.r2dbc.repository.Query
-import org.springframework.data.repository.kotlin.CoroutineSortingRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface RoleRepo : CoroutineSortingRepository<Role, UUID> {
+interface RoleRepo : CoroutinePagingRepository<Role, String, UUID> {
     @Query("""select * from "role" order by created_at limit :first""")
-    suspend fun findAll(first: Int): Flow<Role>
+    override suspend fun findAll(first: Int): Flow<Role>
 
     @Query(
         """
@@ -26,10 +25,10 @@ interface RoleRepo : CoroutineSortingRepository<Role, UUID> {
             )
         """
     )
-    suspend fun findAll(first: Int, after: String): Flow<Role>
+    override suspend fun findAll(first: Int, after: String): Flow<Role>
 
     @Query("""select row_number() over (order by created_at) from "role" where name = :name limit 1""")
-    suspend fun findIndex(name: String): Long
+    override suspend fun findIndex(name: String): Long
 
     @Query("""select * from "role" where name = :name limit 1""")
     suspend fun findByName(name: String): Role?
--- server/diekeditora-infra/src/main/kotlin/repositories/UserRepo.kt
+++ server/diekeditora-infra/src/main/kotlin/repositories/UserRepo.kt
@@ -3,14 +3,13 @@ package com.diekeditora.infra.repositories
 import com.diekeditora.domain.user.User
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.r2dbc.repository.Query
-import org.springframework.data.repository.kotlin.CoroutineSortingRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface UserRepo : CoroutineSortingRepository<User, UUID> {
+interface UserRepo : CoroutinePagingRepository<User, String, UUID> {
     @Query("""select * from "user" order by created_at limit :first""")
-    suspend fun findAll(first: Int): Flow<User>
+    override suspend fun findAll(first: Int): Flow<User>
 
     @Query(
         """
@@ -26,10 +25,10 @@ interface UserRepo : CoroutineSortingRepository<User, UUID> {
             )
         """
     )
-    suspend fun findAll(first: Int, after: String): Flow<User>
+    override suspend fun findAll(first: Int, after: String): Flow<User>
 
     @Query("""select row_number() over (order by created_at) from "user" where username = :username limit 1""")
-    suspend fun findIndex(username: String): Long
+    override suspend fun findIndex(username: String): Long
 
     @Query("""select * from "user" where username = :username limit 1""")
     suspend fun findByUsername(username: String): User?
