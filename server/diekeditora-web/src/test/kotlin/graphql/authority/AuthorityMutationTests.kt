263394c229bfcdf342b0455920d6c6dd9e40f7a1
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-21 03:19:49 -0300

    fix(server): fix linking authorities

---
+++ server/diekeditora-web/src/test/kotlin/graphql/authority/AuthorityMutationTests.kt
@@ -0,0 +1,83 @@
+package com.diekeditora.web.tests.graphql.authority
+
+import com.diekeditora.domain.authority.AuthorityService
+import com.diekeditora.domain.page.asNodeList
+import com.diekeditora.domain.role.RoleService
+import com.diekeditora.domain.user.UserService
+import com.diekeditora.infra.entities.Authority
+import com.diekeditora.web.tests.factories.AuthorityFactory
+import com.diekeditora.web.tests.factories.RoleFactory
+import com.diekeditora.web.tests.factories.UserFactory
+import com.diekeditora.web.tests.graphql.GraphQLTestClient
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+@SpringBootTest
+class AuthorityMutationTests(
+    @Autowired val userService: UserService,
+    @Autowired val userFactory: UserFactory,
+    @Autowired val roleService: RoleService,
+    @Autowired val roleFactory: RoleFactory,
+    @Autowired val authorityService: AuthorityService,
+    @Autowired val authorityFactory: AuthorityFactory,
+    @Autowired val client: GraphQLTestClient,
+) {
+
+    @Test
+    fun `test should link authorities to role`(): Unit = runBlocking {
+        val first = 15
+
+        val role = roleService.saveRole(roleFactory.create())
+        val currentAuthorities = authorityService.findAuthoritiesByRole(role, first)
+
+        val newAuthorities = authorityFactory.createMany(first).map(Authority::value)
+
+        val response = client.request(LinkAuthoritiesToRoleMutation(role.name, newAuthorities)) {
+            authenticate("authority.admin")
+        }
+
+        assertNotNull(response)
+        assertEquals(0, currentAuthorities.edges.size)
+        assertTrue(
+            newAuthorities.containsAll(
+                authorityService.findAuthoritiesByRole(role, first).asNodeList(),
+            ),
+        )
+    }
+
+    @Test
+    fun `test should unlink authorities from role`(): Unit = runBlocking {
+    }
+
+    @Test
+    fun `test should link authorities to user`(): Unit = runBlocking {
+        val first = 15
+
+        val user = userService.saveUser(userFactory.create())
+        val currentAuthorities = authorityService.findAuthoritiesByUser(user, first)
+
+        val newAuthorities = authorityFactory.createMany(first).map(Authority::value)
+
+        val response =
+            client.request(LinkAuthoritiesToUserMutation(user.username, newAuthorities)) {
+                authenticate("authority.admin")
+            }
+
+        assertNotNull(response)
+        assertEquals(0, currentAuthorities.edges.size)
+        assertTrue(
+            newAuthorities.containsAll(
+                authorityService.findAuthoritiesByUser(user, first).asNodeList(),
+            ),
+        )
+    }
+
+    @Test
+    fun `test should unlink authorities from user`(): Unit = runBlocking {
+    }
+}
---
+++ server/diekeditora-web/src/test/kotlin/graphql/authority/AuthorityQueries.kt
@@ -0,0 +1,87 @@
+package com.diekeditora.web.tests.graphql.authority
+
+import com.diekeditora.domain.page.AppPage
+import com.diekeditora.domain.role.Role
+import com.diekeditora.domain.user.User
+import com.diekeditora.web.tests.graphql.TestQuery
+
+data class AuthoritiesQuery(val first: Int, val after: String? = null) : TestQuery<AppPage<String>>(
+    """
+    query(${"\$first"}: Int!, ${"\$after"}: String) {
+        authorities(first: ${"\$first"}, after: ${"\$after"}) {
+            pageInfo {
+                hasNextPage
+                hasPreviousPage
+                startCursor
+                endCursor
+                totalPages
+            }
+            
+            edges {
+                cursor
+                node
+            }
+        }
+    }
+    """.trimIndent()
+)
+
+data class LinkAuthoritiesToUserMutation(val username: String, val authorities: List<String>) :
+    TestQuery<User?>(
+        """
+        mutation(${"\$username"}: String!, ${"\$authorities"}: [String!]!) {
+            linkAuthoritiesToUser(username: ${"\$username"}, authorities: ${"\$authorities"}) {
+                name
+                email
+                username
+                birthday
+                createdAt
+                updatedAt
+                deletedAt
+            }
+        }
+        """.trimIndent()
+    )
+
+data class UnlinkAuthoritiesFromUserMutation(val name: String, val authorities: List<String>) :
+    TestQuery<Role?>(
+        """
+        mutation(${"\$username"}: String!, ${"\$authorities"}: [String!]!) {
+            unlinkAuthoritiesFromUser(username: ${"\$username"}, authorities: ${"\$authorities"}) {
+                name
+                email
+                username
+                birthday
+                createdAt
+                updatedAt
+                deletedAt
+            }
+        }
+        """.trimIndent()
+    )
+
+data class LinkAuthoritiesToRoleMutation(val name: String, val authorities: List<String>) :
+    TestQuery<Role?>(
+        """
+        mutation(${"\$name"}: String!, ${"\$authorities"}: [String!]!) {
+            linkAuthoritiesToRole(name: ${"\$name"}, authorities: ${"\$authorities"}) {
+                name
+                createdAt
+                updatedAt
+            }
+        }
+        """.trimIndent()
+    )
+
+data class UnlinkAuthoritiesFromRoleMutation(val name: String, val authorities: List<String>) :
+    TestQuery<Role?>(
+        """
+        mutation(${"\$name"}: String!, ${"\$authorities"}: [String!]!) {
+            unlinkAuthoritiesFromRole(name: ${"\$name"}, authorities: ${"\$authorities"}) {
+                name
+                createdAt
+                updatedAt
+            }
+        }
+        """.trimIndent()
+    )
