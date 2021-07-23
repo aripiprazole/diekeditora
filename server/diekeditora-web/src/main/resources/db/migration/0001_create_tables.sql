fc3c377aa5ed6e04fd480685d086c9d66d0f7b8d
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-07-23 03:53:46 -0300

    fix(server): remove remaining fields from user

--- server/diekeditora-web/src/main/resources/db/migration/0001_create_tables.sql
+++ server/diekeditora-web/src/main/resources/db/migration/0001_create_tables.sql
@@ -4,10 +4,8 @@ create table if not exists "user"
     name              varchar(120)             not null,
     username          varchar(50) unique       not null,
     email             varchar(120) unique      not null,
-    password          varchar(255)                      default null,
     birthday          timestamp with time zone          default null,
     updated_at        timestamp with time zone          default null,
-    email_verified_at timestamp with time zone          default null,
     deleted_at        timestamp with time zone          default null,
     created_at        timestamp with time zone not null default current_timestamp,
     primary key (id)
