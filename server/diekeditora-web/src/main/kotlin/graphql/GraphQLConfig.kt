3814ac54005240d6db998b9732db58e10f2d5ee6
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-07-27 09:56:42 -0300

    feat(server): implement file uploading in graphql endpoint

--- server/diekeditora-web/src/main/kotlin/graphql/GraphQLConfig.kt
+++ server/diekeditora-web/src/main/kotlin/graphql/GraphQLConfig.kt
@@ -1,13 +1,16 @@
 package com.diekeditora.web.graphql
 
 import com.diekeditora.domain.id.UniqueId
+import com.diekeditora.domain.image.Upload
 import com.expediagroup.graphql.generator.execution.FlowSubscriptionExecutionStrategy
 import com.expediagroup.graphql.generator.hooks.SchemaGeneratorHooks
 import com.fasterxml.jackson.databind.ObjectMapper
 import graphql.execution.ExecutionStrategy
+import graphql.schema.Coercing
 import graphql.schema.GraphQLScalarType.newScalar
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.multipart.FilePart
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -19,7 +22,30 @@ class GraphQLConfig(val objectMapper: ObjectMapper) {
     fun subscriptionExecutionStrategy(): ExecutionStrategy = FlowSubscriptionExecutionStrategy()
 
     @Bean
+    @OptIn(ExperimentalStdlibApi::class)
     fun hooks(): SchemaGeneratorHooks = GraphQLSchemaHooks(objectMapper).apply {
+        withScalar<Upload>(
+            newScalar()
+                .name("Upload")
+                .coercing(object : Coercing<Upload, Unit> {
+                    override fun serialize(dataFetcherResult: Any?) {
+                        error("Could not parse literal Upload")
+                    }
+
+                    override fun parseValue(input: Any?): Upload {
+                        val part = input as? FilePart
+                            ?: error("Could not parse a non-file part object in Upload scalar")
+
+                        return Upload(part)
+                    }
+
+                    override fun parseLiteral(input: Any?): Upload {
+                        error("Could not parse literal Upload")
+                    }
+                })
+                .build()
+        )
+
         withScalar<UniqueId>(
             newScalar()
                 .name("UniqueId")
