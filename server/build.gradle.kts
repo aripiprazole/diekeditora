a58ac2f8e08727ac1a7a4acdedddfc3c56fb2cf6
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-07-28 03:34:50 -0300

    refactor(server): remove prefix from modules' names and change web to app

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,6 +1,5 @@
 import com.diekeditora.build.kotlin
 import com.diekeditora.build.libs
-import com.diekeditora.build.projects
 
 buildscript {
     repositories {
@@ -25,10 +24,6 @@ version = "0.0.1-SNAPSHOT"
 
 dependencies {
     ktlintRuleset(libs.pinterest.ktlint)
-    implementation(projects.web)
-    implementation(projects.domain)
-    implementation(projects.infra)
-    implementation(projects.shared)
 }
 
 application {
