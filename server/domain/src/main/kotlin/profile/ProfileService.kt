8eb86474fac0518a2df0998f46090bdea0cdf8c5
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-07-28 04:31:37 -0300

    refactor(server): remove findProfiles from ProfileService

--- server/domain/src/main/kotlin/profile/ProfileService.kt
+++ server/domain/src/main/kotlin/profile/ProfileService.kt
@@ -4,8 +4,6 @@ import com.diekeditora.domain.user.User
 import graphql.relay.Connection
 
 interface ProfileService {
-    suspend fun findProfiles(first: Int, after: String? = null): Connection<Profile>
-
     suspend fun findOrCreateProfileByUser(user: User): Profile
 
     suspend fun updateProfile(profile: Profile): Profile
