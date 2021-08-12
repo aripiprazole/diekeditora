04fc20e1afd0c3e3f591814d5757dc485debb1d4
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-08-12 00:31:48 -0300

    fix(web): fix not importing outside of source modules in app

--- web/packages/app/next.config.js
+++ web/packages/app/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require('next-transpile-modules')(['@diekeditora/store'], {resolveSymlinks: false});
+const withTM = require('next-transpile-modules')(['@diekeditora/store'], {resolveSymlinks: true});
 
 module.exports = withTM({
   pageExtensions: ['tsx'],
