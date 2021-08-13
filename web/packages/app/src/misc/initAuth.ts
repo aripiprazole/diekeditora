e882c4072762d94545f032e6a773edfb3933f78b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-08-13 17:03:53 -0300

    feat(web): add login/logout endpoint

--- web/packages/app/src/misc/initAuth.ts
+++ web/packages/app/src/misc/initAuth.ts
@@ -1,5 +1,4 @@
 import {init} from 'next-firebase-auth';
-import * as process from 'process';
 
 export default function initAuth() {
   try {
@@ -13,17 +12,17 @@ export default function initAuth() {
         credential: {
           privateKey: process.env.FIREBASE_PRIVATE_KEY,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
+          projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         },
       },
       firebaseClientInitConfig: {
-        apiKey: process.env.NEXT_APP_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_APP_FIREBASE_APP_ID,
-        measurementId: process.env.NEXT_APP_FIREBASE_MEASUREMENT_ID,
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
       },
       cookies: {
         name: 'DiekEditora', // required
