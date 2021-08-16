f26123b11b95fcc45700b698ba179e0195dcc1d6
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-08-16 17:08:58 -0300

    refactor(server): add targets to directives

--- server/domain/src/main/kotlin/graphql/Min.kt
+++ server/domain/src/main/kotlin/graphql/Min.kt
@@ -1,7 +1,9 @@
 package com.diekeditora.domain.graphql
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDirective
+import graphql.introspection.Introspection.DirectiveLocation.ARGUMENT_DEFINITION
 import graphql.introspection.Introspection.DirectiveLocation.FIELD_DEFINITION
+import graphql.introspection.Introspection.DirectiveLocation.INPUT_FIELD_DEFINITION
 
-@GraphQLDirective(locations = [FIELD_DEFINITION])
+@GraphQLDirective(locations = [FIELD_DEFINITION, INPUT_FIELD_DEFINITION, ARGUMENT_DEFINITION])
 annotation class Min(val value: Int, val message: String = "")
