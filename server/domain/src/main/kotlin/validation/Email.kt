a4b984f6f782db7c92b398b89ed17f848ec3fd3e
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-08-16 17:38:36 -0300

    feat(server): add default messages and ignore it

--- server/domain/src/main/kotlin/validation/Email.kt
+++ server/domain/src/main/kotlin/validation/Email.kt
@@ -1,9 +1,10 @@
 package com.diekeditora.domain.validation
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDirective
+import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
 import graphql.introspection.Introspection.DirectiveLocation.ARGUMENT_DEFINITION
 import graphql.introspection.Introspection.DirectiveLocation.FIELD_DEFINITION
 import graphql.introspection.Introspection.DirectiveLocation.INPUT_FIELD_DEFINITION
 
 @GraphQLDirective(locations = [FIELD_DEFINITION, INPUT_FIELD_DEFINITION, ARGUMENT_DEFINITION])
-annotation class Email(val message: String = "")
+annotation class Email(@GraphQLIgnore val message: String = "The string input must be an email")
--- server/domain/src/main/kotlin/validation/Regex.kt
+++ server/domain/src/main/kotlin/validation/Regex.kt
@@ -1,10 +1,14 @@
 package com.diekeditora.domain.validation
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDirective
+import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
 import graphql.introspection.Introspection.DirectiveLocation.ARGUMENT_DEFINITION
 import graphql.introspection.Introspection.DirectiveLocation.FIELD_DEFINITION
 import graphql.introspection.Introspection.DirectiveLocation.INPUT_FIELD_DEFINITION
 import org.intellij.lang.annotations.Language
 
 @GraphQLDirective(locations = [FIELD_DEFINITION, INPUT_FIELD_DEFINITION, ARGUMENT_DEFINITION])
-annotation class Regex(@Language("RegExp") val regex: String)
+annotation class Regex(
+    @Language("RegExp") val regex: String,
+    @GraphQLIgnore val message: String = "The string input not valid"
+)
