e2a2373610d7bb76795a4f393ca06d30957f062c
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-08-16 17:12:48 -0300

    feat(server): add Size directive

--- server/domain/src/main/kotlin/chapter/ChapterInput.kt
+++ server/domain/src/main/kotlin/chapter/ChapterInput.kt
@@ -1,15 +1,15 @@
 package com.diekeditora.domain.chapter
 
 import com.diekeditora.domain.graphql.Future
-import com.diekeditora.domain.graphql.Max
 import com.diekeditora.domain.graphql.Min
 import com.diekeditora.domain.graphql.NotBlank
+import com.diekeditora.domain.graphql.Size
 import com.diekeditora.domain.id.UniqueId
 import java.time.LocalDate
 
 @Suppress("Detekt.MagicNumber")
 data class ChapterInput(
-    @NotBlank @Max(32) @Min(4) val title: String,
+    @NotBlank @Size(min = 4, max = 32) val title: String,
     @Min(0) val index: Int,
     @Future val releasedOn: LocalDate? = null,
     val enabled: Boolean = false,
---
+++ server/domain/src/main/kotlin/graphql/Size.kt
@@ -0,0 +1,9 @@
+package com.diekeditora.domain.graphql
+
+import com.expediagroup.graphql.generator.annotations.GraphQLDirective
+import graphql.introspection.Introspection.DirectiveLocation.ARGUMENT_DEFINITION
+import graphql.introspection.Introspection.DirectiveLocation.FIELD_DEFINITION
+import graphql.introspection.Introspection.DirectiveLocation.INPUT_FIELD_DEFINITION
+
+@GraphQLDirective(locations = [FIELD_DEFINITION, INPUT_FIELD_DEFINITION, ARGUMENT_DEFINITION])
+annotation class Size(val min: Int = -1, val max: Int = -1, val message: String = "")
