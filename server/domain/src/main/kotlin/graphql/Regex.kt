563c7bd92791a15aed492b3d4a66d827152a2889
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-08-16 17:08:58 -0300

    refactor(server): add targets to directives

--- server/domain/src/main/kotlin/graphql/Regex.kt
+++ server/domain/src/main/kotlin/graphql/Regex.kt
@@ -1,8 +1,10 @@
 package com.diekeditora.domain.graphql
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDirective
+import graphql.introspection.Introspection.DirectiveLocation.ARGUMENT_DEFINITION
 import graphql.introspection.Introspection.DirectiveLocation.FIELD_DEFINITION
+import graphql.introspection.Introspection.DirectiveLocation.INPUT_FIELD_DEFINITION
 import org.intellij.lang.annotations.Language
 
-@GraphQLDirective(locations = [FIELD_DEFINITION])
+@GraphQLDirective(locations = [FIELD_DEFINITION, INPUT_FIELD_DEFINITION, ARGUMENT_DEFINITION])
 annotation class Regex(@Language("RegExp") val regex: String)
