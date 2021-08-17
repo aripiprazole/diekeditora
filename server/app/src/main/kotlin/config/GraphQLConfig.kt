ae47797abd1182993b1593cc365a20cb96ee9e43
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-08-17 00:41:52 -0300

    feat(server): add a good validation error presentation for validation errors

--- server/app/src/main/kotlin/config/GraphQLConfig.kt
+++ server/app/src/main/kotlin/config/GraphQLConfig.kt
@@ -1,6 +1,8 @@
 package com.diekeditora.app.config
 
+import com.diekeditora.app.graphql.GraphQLExceptionHandler
 import com.expediagroup.graphql.generator.execution.FlowSubscriptionExecutionStrategy
+import graphql.execution.DataFetcherExceptionHandler
 import graphql.execution.ExecutionStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,5 +10,10 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class GraphQLConfig {
     @Bean
-    fun subscriptionExecutionStrategy(): ExecutionStrategy = FlowSubscriptionExecutionStrategy()
+    fun dataFetcherExceptionHandler(): DataFetcherExceptionHandler =
+        GraphQLExceptionHandler()
+
+    @Bean
+    fun subscriptionExecutionStrategy(exceptionHandler: DataFetcherExceptionHandler): ExecutionStrategy =
+        FlowSubscriptionExecutionStrategy(exceptionHandler)
 }
