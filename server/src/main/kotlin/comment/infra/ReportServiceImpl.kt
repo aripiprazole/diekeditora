65798f82df155c620b75a48f664c06c270ea6994
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-09-17 04:30:36 -0300

    fix(server): add stubs

---
+++ server/src/main/kotlin/comment/infra/ReportServiceImpl.kt
@@ -0,0 +1,19 @@
+package com.diekeditora.comment.infra
+
+import com.diekeditora.comment.domain.Comment
+import com.diekeditora.comment.domain.ReportReason
+import com.diekeditora.comment.domain.ReportService
+import com.diekeditora.id.domain.UniqueId
+import graphql.relay.Connection
+import org.springframework.stereotype.Service
+
+@Service
+internal class ReportServiceImpl : ReportService {
+    override suspend fun findReportedComments(first: Int, after: UniqueId?): Connection<Comment> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun reportComment(comment: Comment, reason: ReportReason): Comment {
+        TODO("Not yet implemented")
+    }
+}
