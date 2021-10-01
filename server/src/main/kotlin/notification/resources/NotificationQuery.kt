be7d92b02e4aefeeaa7e8de33af3a4bf57baf82d
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-10-01 08:04:22 -0300

    fix(server): fix import errors in NotificationQuery

--- server/src/main/kotlin/notification/resources/NotificationQuery.kt
+++ server/src/main/kotlin/notification/resources/NotificationQuery.kt
@@ -1,7 +1,12 @@
 package com.diekeditora.notification.resources
 
+import com.diekeditora.graphql.infra.AuthGraphQLContext
+import com.diekeditora.id.domain.UniqueId
+import com.diekeditora.notification.domain.Notification
 import com.diekeditora.notification.domain.NotificationService
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import com.expediagroup.graphql.server.operations.Query
+import graphql.relay.Connection
 import org.springframework.stereotype.Component
 
 @Component
